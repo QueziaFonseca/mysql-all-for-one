c7edcb77c2eb8acb6cc0c5e215035a902be626ca
quezia-lima <qfonseca1@gmail.com>
2022-04-06 23:11:28 -0300

    Desafio 21

--- desafio21.sql
+++ desafio21.sql
@@ -3,3 +3,5 @@ discount, status_id,  date_allocated, purchase_order_id, inventory_id ) VALUES
 (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129),
 (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129)
 ;
+
+# https://www.mysqltutorial.org/mysql-insert-statement.aspx
